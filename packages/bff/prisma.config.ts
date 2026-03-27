153f8f43e576b7880de7d9883bbec69ef3145dee
Carlos Alaniz <carlosglvn93@gmail.com>
2026-03-27 13:44:21 -0500

    .

--- packages/bff/prisma.config.ts
+++ packages/bff/prisma.config.ts
@@ -1,12 +1,11 @@
 import path from 'node:path';
 import { defineConfig } from 'prisma/config';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 export default defineConfig({
-  earlyAccess: true,
   schema: path.join(__dirname, 'prisma', 'schema.prisma'),
-  migrate: {
-    migrations: path.join(__dirname, 'prisma', 'migrations'),
-  },
   datasource: {
     url: process.env.DATABASE_URL!,
   },
